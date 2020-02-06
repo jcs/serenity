7c7427faa01f46a9fa33e6d094d2f2372d55542e
joshua stein <jcs@jcs.org>
2020-02-06 10:27:11 -0600

    LibCore: Implement Core::ProcessStatisticsReader on OpenBSD

--- Libraries/LibCore/ProcessStatisticsReader.cpp
+++ Libraries/LibCore/ProcessStatisticsReader.cpp
@@ -32,19 +32,130 @@
 #include <pwd.h>
 #include <stdio.h>
 
+#ifdef __OpenBSD__
+#include <sys/ioctl.h>
+#include <sys/param.h>
+#define PLEDGENAMES
+#include <sys/pledge.h>
+#include <sys/proc.h>
+#include <sys/resource.h>
+#include <sys/stat.h>
+#include <sys/sysctl.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <kvm.h>
+#include <limits.h>
+#include <string.h>
+#endif
+
 namespace Core {
 
 HashMap<uid_t, String> ProcessStatisticsReader::s_usernames;
 
 HashMap<pid_t, Core::ProcessStatistics> ProcessStatisticsReader::get_all()
 {
+    HashMap<pid_t, Core::ProcessStatistics> map;
+
+#ifdef __OpenBSD__
+    kvm_t *kd;
+    struct kinfo_proc *kp;
+    struct kinfo_file *kf;
+    char errbuf[_POSIX2_LINE_MAX];
+    int nentries, i;
+
+    kd = kvm_openfiles(nullptr, nullptr, nullptr, KVM_NO_FILES, errbuf);
+    kp = kvm_getprocs(kd, KERN_PROC_ALL, 0, sizeof(*kp), &nentries);
+    if (kp == NULL) {
+        perror("kvm_getprocs");
+        ASSERT_NOT_REACHED();
+    }
+
+    for (i = 0; i < nentries; i++) {
+        Core::ProcessStatistics process;
+        char *ttname;
+        int nfiles, j;
+
+        memset(&process, 0, sizeof(process));
+
+        process.pid = kp[i].p_pid;
+        process.pgid = kp[i].p__pgid;
+        process.pgp = kp[i].p_tpgid;
+        process.sid = kp[i].p_sid;
+        process.uid = kp[i].p_uid;
+        process.gid = kp[i].p_gid;
+        process.ppid = kp[i].p_gid;
+
+        if ((kf = kvm_getfiles(kd, KERN_FILE_BYPID, process.pid, sizeof(*kf), &nfiles)) != NULL)
+            process.nfds = nfiles;
+
+        process.name = String(kp[i].p_comm);
+        if ((int)kp[i].p_tdev != (int)NODEV && (ttname = devname(kp[i].p_tdev, S_IFCHR)))
+            process.tty = String(ttname);
+
+        StringBuilder pledge;
+
+        for (j = 0; pledgenames[j].bits != 0; j++) {
+            if (pledgenames[j].bits & kp[i].p_pledge) {
+                if (pledge.length())
+                    pledge.append(' ');
+                pledge.append(pledgenames[j].name);
+            }
+        }
+
+        process.pledge = pledge.to_string();
+
+        if (kp[i].p_eflag & EPROC_UNVEIL) {
+            if (kp[i].p_eflag & EPROC_LKUNVEIL)
+                process.veil = String("Locked");
+            else
+                process.veil = String("Dropped");
+        } else
+            process.veil = String("None");
+
+        process.amount_virtual = (kp[i].p_vm_dsize + kp[i].p_vm_ssize + kp[i].p_vm_tsize) * getpagesize();
+        process.amount_resident = kp[i].p_vm_rssize * getpagesize();
+        // no shared info
+
+        Core::ThreadStatistics thread;
+        memset(&thread, 0, sizeof(thread));
+
+        thread.tid = kp[i].p_pid;
+        thread.name = String(kp[i].p_comm);
+
+        switch (kp[i].p_stat) {
+        case SSTOP:
+            thread.state = String("Stopped");
+            break;
+        case SSLEEP:
+            if (kp[i].p_flag & P_SINTR)
+                thread.state = String("Sleeping");
+            else
+                thread.state = String("Disk");
+            break;
+        case SRUN:
+        case SIDL:
+        case SONPROC:
+            thread.state = "Runnable";
+            break;
+        case SDEAD:
+            thread.state = "Dead";
+            break;
+        default:
+            thread.state = "Invalid";
+        }
+
+        process.threads.append(move(thread));
+
+        process.username = username_from_uid(process.uid);
+        map.set(process.pid, process);
+    }
+
+#else
     auto file = Core::File::construct("/proc/all");
     if (!file->open(Core::IODevice::ReadOnly)) {
         fprintf(stderr, "ProcessStatisticsReader: Failed to open /proc/all: %s\n", file->error_string());
         return {};
     }
-
-    HashMap<pid_t, Core::ProcessStatistics> map;
 
     auto file_contents = file->read_all();
     auto json = JsonValue::from_string(file_contents);
@@ -103,6 +214,7 @@ HashMap<pid_t, Core::ProcessStatistics> ProcessStatisticsReader::get_all()
         process.username = username_from_uid(process.uid);
         map.set(process.pid, process);
     });
+#endif
 
     return map;
 }
